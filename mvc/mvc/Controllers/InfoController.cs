02da8f84d7322c3fc1b53cfa431c849002ba6820
Your Name <you@example.com>
2024-11-20 11:19:10 +0200

    initial commit

---
+++ mvc/mvc/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
